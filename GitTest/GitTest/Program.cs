c2f8cab3029f15f4a4539156d3abaa90f38bea1b
L-Henry <Henry.Ludwig@telenet.be>
2019-07-08 11:07:42 +0200

    Som update

--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -7,6 +7,12 @@ namespace GitTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello All!");
+            Console.WriteLine(Som(5,5));
+        }
+
+        static int Som(int g1, int g2)
+        {
+            return g1 + g2;
         }
     }
 }
